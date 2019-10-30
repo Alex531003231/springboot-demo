c67be527ef41b6225db14fb40bb09a15ad7463d9
haibo.li01@hand-china.com <531003231@qq.com>
2019-10-30 11:29:50 +0800

    [ADD]新建工程:springBoot功能扩展

---
+++ demo-expansion/src/main/java/com/lhb/demoexpansion/DemoExpansionApplication.java
@@ -0,0 +1,13 @@
+package com.lhb.demoexpansion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoExpansionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoExpansionApplication.class, args);
+    }
+
+}
